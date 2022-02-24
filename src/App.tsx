09ebe286e3497080967abdf85bfd6233bb9fb67f
Jonathan Lassiter <36551695+LassiterJ@users.noreply.github.com>
2022-02-23 23:00:19 -0500

    First mui components, initial MUI setup complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import {SamplePage} from "./components/sample-page/SamplePage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export const App = () =>  {
+
+    return (<div className="App">
+<CssBaseline>
+      <SamplePage />
+</CssBaseline>
     </div>
-  );
+);
 }
 
-export default App;
+
